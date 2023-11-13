45af9c5e752652c3b3781926f5c38d7070626d59
Md Kaish Alam <alamkaishg1511@gmail.com>
2023-11-13 21:06:05 +0530

    dark bg bug clear

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,8 +4,8 @@ import Heroes from "./_components/Heroes";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10 dark:bg-[#11101d]">
+    <div className="min-h-full flex flex-col dark:bg-[#11101d]">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
       </div>
